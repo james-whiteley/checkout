6d53dc36509ba8a4765c8f7a2d1bac149bf82cfe
James Whiteley <jameswhiteley@Jamess-MacBook-Air.local>
2020-12-12 16:37:16 +0000

    Added doc comments, generated docs and added appropriate files to .gitignore

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,15 +1,28 @@
+# Public: Various methods used to scan/add items to a basket
+# and calculate total cost of basket.
 class Checkout
+  # Internal: Returns the hash of item prices.
   attr_reader :prices
   private :prices
 
+  # Public: Initialize item prices.
+  #
+  # prices - A hash of item prices.
   def initialize(prices)
     @prices = prices
   end
 
+  # Public: Add item to basket.
+  #
+  # item  - Item name to be added to basket.
   def scan(item)
     basket << item.to_sym
   end
 
+  # Public: Calculate total price of items in the basket,
+  # determining if item should have discount applied.
+  #
+  # Returns the basket total.
   def total
     total = 0
 
@@ -37,6 +50,9 @@ class Checkout
 
   private
 
+  # Internal: Get basket array or initialise if does not exist
+  #
+  # Returns the array of items in the basket.
   def basket
     @basket ||= Array.new
   end
